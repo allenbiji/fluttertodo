19941560287ebafa2b52f38c68a347f6b34120e8
allenbiji <georgeallen378@gmail.com>
2025-01-06 00:20:01 +0530

    todo model mod

--- todoapp/lib/models/todo.dart
+++ todoapp/lib/models/todo.dart
@@ -5,6 +5,14 @@ class Todo {
 
   Todo({required this.id, required this.name, this.isCompleted = false});
 
+  factory Todo.fromJson(Map<String, dynamic> json) {
+    return Todo(
+      id: json['_id'],
+      name: json['name'],
+      isCompleted: json['isCompleted'] ?? false,
+    );
+  }
+
   Todo toggleCompletion() {
     return Todo(
       id: id,
